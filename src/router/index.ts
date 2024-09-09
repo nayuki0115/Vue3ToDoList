20d661bb13e73c0e3a2f14af8d3c93c474d9f4a3
Annie Wu <annie25506@gmail.com>
2024-09-09 23:58:56 +0800

    fix: typescript error

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
+interface Meta {
+  title?: string;
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -15,7 +19,8 @@ const router = createRouter({
 
 
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title || 'Vue3 To Do List'
+  const meta = to.meta as Meta;
+  document.title = meta.title || 'Vue3 To Do List';
   next()
 })
 
